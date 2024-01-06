b40ca190ea8f0909dd12012b77a0abe743e89931
riteshkshik <stevenfrayne12344@gmail.com>
2024-01-06 13:38:40 +0530

    form-filler extension basic funtionality achieved

---
+++ content.js
@@ -0,0 +1,27 @@
+
+
+
+console.log("we are on google .com")
+// console.log("ritesh -> ",document.activeElement)
+
+
+
+document.addEventListener("keydown", function (event) {
+  console.log("key down");
+  if (event.ctrlKey && event.key === " ") {
+    const shortKey = document.activeElement.value;
+    replaceValue(shortKey);
+  }
+});
+
+function replaceValue(shortKey) {
+  chrome.storage.local.get(shortKey, function (result) {
+    var value = result[shortKey];
+    // console.log("Value retrieved from chrome.storage.local:", value);
+    if (value) {
+      document.activeElement.value = value;
+    } else {
+      console.log("Value not found for key:", shortKey);
+    }
+  });
+}
