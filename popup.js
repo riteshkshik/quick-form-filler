387e41342a63b1bf7c1cba4f3bd34fe46150d830
riteshkshik <stevenfrayne12344@gmail.com>
2024-01-08 00:26:52 +0530

    popup ui done

--- popup.js
+++ popup.js
@@ -4,11 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("addButton")
     .addEventListener("click", addKeyValuePair);
+
+  document
+    .getElementById("view-shortcut-btn")
+    .addEventListener("click", openNewPage);
 });
 
 function addKeyValuePair() {
+  
   var key = document.getElementById("key").value.trim();
   var value = document.getElementById("value").value.trim();
+
+  if (!key) {
+    alert("Key Can't be Empty!");
+    return;
+  }
+  if (!value) {
+    alert("Value Can't be Empty!");
+    return;
+  }
+  
 
   chrome.storage.local.set({ [key]: value }, function () {
     console.log("Key-value pair added to chrome.storage.local:", key, value);
@@ -16,4 +31,8 @@ function addKeyValuePair() {
   
   document.getElementById("key").value = "";
   document.getElementById("value").value = "";
+}
+
+function openNewPage(){
+  chrome.tabs.create({ url: "list.html" });
 }
